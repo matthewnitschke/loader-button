7733840012450829d01057e697f04d85fe7669de
Matthew Nitschke <mnitschke@faithchapel.cc>
2016-05-16 16:03:52 -0600

    Initial Commit

---
+++ loaderButton.js
@@ -0,0 +1,16 @@
+;(function(ns){
+  ns.spinnerHtml = "<div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div>";
+
+  ns.add = function add(buttonId){
+    var button = document.getElementById(buttonId);
+    button.setAttribute('data-button-text', button.innerHTML);
+    button.innerHTML = ns.spinnerHtml;
+  }
+
+  ns.remove = function remove(buttonId){
+    var button = document.getElementById(buttonId);
+    var btnText = button.getAttribute('data-button-text');
+    button.innerHTML = btnText;
+  }
+
+}(this.IdleListener = this.IdleListener || {}));
