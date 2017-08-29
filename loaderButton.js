c715da9feb8bf83be8b512e094e4ac28588770f7
Matthew Nitschke <matthewcnitschke@gmail.com>
2017-08-29 17:59:09 -0600

    Implemented selector

--- loaderButton.js
+++ loaderButton.js
@@ -1,8 +1,16 @@
 ;(function(ns){
   ns.spinnerHtml = "<div class='loader-button'><div class='bounce1 loader-button-dot'></div><div class='bounce2 loader-button-dot'></div><div class='bounce3 loader-button-dot'></div></div>";
 
+  function getButtonElement(selector){
+    if (typeof selector === "string"){
+      return document.querySelector(selector);
+    } else {
+      return selector;
+    }
+  }
+
   ns.add = function add(buttonSelector){
-    var button = document.querySelector(buttonSelector);
+    var button = getButtonElement(buttonSelector);
     button.setAttribute('data-button-text', button.innerHTML);
     button.setAttribute('disabled', true);
     button.innerHTML = ns.spinnerHtml;
@@ -11,12 +19,7 @@
   }
 
   ns.remove = function remove(buttonSelector){
-    var button;
-    if (typeof buttonSelector === "string"){
-      button = document.querySelector(buttonSelector);
-    } else {
-      button = buttonSelector;
-    }
+    var button = getButtonElement(buttonSelector);
     var btnText = button.getAttribute('data-button-text');
     button.removeAttribute('disabled');
     button.innerHTML = btnText;
