19deb60dcd1a15885e0d84654f63b1d2eaafcfad
Matthew Nitschke <matthewcnitschke@gmail.com>
2017-08-29 17:52:03 -0600

    Minor updates

--- loaderButton.js
+++ loaderButton.js
@@ -1,15 +1,22 @@
 ;(function(ns){
-  ns.spinnerHtml = "<div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div>";
+  ns.spinnerHtml = "<div class='loader-button'><div class='bounce1 loader-button-dot'></div><div class='bounce2 loader-button-dot'></div><div class='bounce3 loader-button-dot'></div></div>";
 
   ns.add = function add(buttonSelector){
     var button = document.querySelector(buttonSelector);
     button.setAttribute('data-button-text', button.innerHTML);
     button.setAttribute('disabled', true);
     button.innerHTML = ns.spinnerHtml;
+
+    return button;
   }
 
   ns.remove = function remove(buttonSelector){
-    var button = document.querySelector(buttonSelector);
+    var button;
+    if (typeof buttonSelector === "string"){
+      button = document.querySelector(buttonSelector);
+    } else {
+      button = buttonSelector;
+    }
     var btnText = button.getAttribute('data-button-text');
     button.removeAttribute('disabled');
     button.innerHTML = btnText;
