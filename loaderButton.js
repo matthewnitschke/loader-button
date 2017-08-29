52bba97ad4887c97f192921abfff5b4a1db59cd1
Matthew Nitschke <matthewcnitschke@gmail.com>
2017-08-29 17:37:09 -0600

    Changed selector from id to queryString

--- loaderButton.js
+++ loaderButton.js
@@ -1,15 +1,15 @@
 ;(function(ns){
   ns.spinnerHtml = "<div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div>";
 
-  ns.add = function add(buttonId){
-    var button = document.getElementById(buttonId);
+  ns.add = function add(buttonSelector){
+    var button = document.querySelector(buttonSelector);
     button.setAttribute('data-button-text', button.innerHTML);
     button.setAttribute('disabled', true);
     button.innerHTML = ns.spinnerHtml;
   }
 
-  ns.remove = function remove(buttonId){
-    var button = document.getElementById(buttonId);
+  ns.remove = function remove(buttonSelector){
+    var button = document.querySelector(buttonSelector);
     var btnText = button.getAttribute('data-button-text');
     button.removeAttribute('disabled');
     button.innerHTML = btnText;
